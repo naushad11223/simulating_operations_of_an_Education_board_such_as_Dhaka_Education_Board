160547f706dfa9484eeb6def577e75e23931e7d3
Naushad <md abu naushad>
2024-12-14 21:35:12 +0600

    naushad

--- src/main/java/com/example/simulating_operations_of_an_education_board_such_as_dhaka_education_board/ChairmanDashboardController.java
+++ src/main/java/com/example/simulating_operations_of_an_education_board_such_as_dhaka_education_board/ChairmanDashboardController.java
@@ -1,4 +1,20 @@
 package com.example.simulating_operations_of_an_education_board_such_as_dhaka_education_board;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class ChairmanDashboardController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }

35cd1b53ab98b17d24b7303724a4c3d28cc629eb
Sabbir030 <2110737@iub.edu.bd>
2024-12-14 11:30:09 +0600

    Add files via upload
    initial commit

---
+++ RequestResultRecheckController.java
@@ -0,0 +1,34 @@
+package Sabbir;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+public class RequestResultRecheckController {
+}
+package com.example.dhakaeducationboard.Sabbir;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RequestResultRecheckFxml {
+
+    @FXML
+    private Button prfMJ;
+
+    @FXML
+    private Button srcFN;
+
+    @FXML
+    void PayRecheckFee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SubmitRecheckRequest(ActionEvent event) {
+
+    }
+
+}
